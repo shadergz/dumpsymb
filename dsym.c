dc35b807a2a51c256ebca1d4df75a52f3e62b04e
Gabriel Correia <gabrielcorreia01012020@hotmail.com>
2022-07-17 22:04:01 -0300

    Using new API

--- dsym.c
+++ dsym.c
@@ -8,30 +8,30 @@ int main(int argc, char *argv[])
     if (argc < 2)
         return argc;
 
-    bin_ctx_t ctx;
-    bin_err_e err = bin_load_file(argv[1], &ctx);
+    BinCtx_t ctx;
+    BinError_t err = BinLoadFile(argv[1], &ctx);
 
     if (err != BIN_E_OK)
-        return fprintf(stderr, "something wrong has heppen: %s\n", bin_error_to_str(err));
+        return fprintf(stderr, "something wrong has heppen: %s\n", BinErrorToStr(err));
     
-    assert(err == bin_get_binary_size(&ctx));
+    assert(err == BinGetLastError(&ctx));
 
-    bin_parser(&ctx);
+    BinParser(&ctx);
 
-    err = bin_get_last_error(&ctx);
+    err = BinGetLastError(&ctx);
     
     if (err != BIN_E_OK)
-        return fprintf(stderr, "something wrong has heppen: %s\n", bin_error_to_str(err));
+        return fprintf(stderr, "something wrong has heppen: %s\n", BinErrorToStr(err));
     
-    printf("%s has been opened for parser\n", bin_get_filename(&ctx));
+    printf("%s has been opened for parser\n", BinGetFilename(&ctx));
 
-    printf("binary size: %ld\n", bin_get_binary_size(&ctx));
+    printf("binary size: %ld\n", BinGetBinarySize(&ctx));
 
     printf("binary type is: %s\n", 
-        bin_binary_type_to_str(bin_get_binary_type(&ctx))
+        BinBinaryTypeToStr(BinGetType(&ctx))
     );
 
-    bin_finish(&ctx);
+    BinFinish(&ctx);
     
 
     return err;
