c8b66cc1da053465651e0197df3f2e8fab2cf520
Gabriel Correia <gabrielcorreia01012020@hotmail.com>
2022-09-03 17:36:58 -0300

    Using new symbols from libbin

--- dsym.c
+++ dsym.c
@@ -13,7 +13,7 @@ int main(int argc, char *argv[])
 
 	bin_load_file(argv[1], &ctx);
 
-	err = bin_get_last_error(&ctx);
+	err = bin_error_get_last(&ctx);
 
 	if (err != BIN_E_OK)
 	{
@@ -21,23 +21,23 @@ int main(int argc, char *argv[])
 	}
 
 	/* Assert test */
-	assert(err == bin_get_last_error(&ctx));
+	assert(err == bin_error_get_last(&ctx));
 
 	bin_parser(&ctx);
 
-	err = bin_get_last_error(&ctx);
+	err = bin_error_get_last(&ctx);
 	
 	if (err != BIN_E_OK)
 	{
 		return fprintf(stderr, "Something wrong has happen: %s\n", bin_error_to_str(err));
 	}
 
-	printf("%s was been opened for parser\n", bin_get_filename(&ctx));
+	printf("%s was been opened for parser\n", bin_obj_get_filename(&ctx));
 
-	printf("Binary size: %ld\n", bin_get_obj_size(&ctx));
+	printf("Binary size: %ld\n", bin_obj_get_size(&ctx));
 
 	printf("Binary type is: %s\n", 
-		bin_obj_type_to_str(bin_get_obj_type(&ctx))
+		bin_obj_type_to_str(bin_obj_get_type(&ctx))
 	);
 
 	/* bin_unload call isn't required */
