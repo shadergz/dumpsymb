1532d60988e1593d1f6a9491e7c3072b7428dac5
Gabriel Correia <gabrielcorreia01012020@hotmail.com>
2022-09-03 16:18:55 -0300

    Indentation applied

--- dsym.c
+++ dsym.c
@@ -5,42 +5,42 @@
 
 int main(int argc, char *argv[])
 {
-    if (argc < 2)
-        return argc;
+	if (argc < 2)
+		return argc;
 
-    BinCtx_t ctx;
-    BinError_t err;
+	BinCtx_t ctx;
+	BinError_t err;
 
-    BinLoadFile(argv[1], &ctx);
+	BinLoadFile(argv[1], &ctx);
 
-    err = BinGetLastError(&ctx);
+	err = BinGetLastError(&ctx);
 
-    if (err != BIN_E_OK)
-    {
-        return fprintf(stderr, "Something wrong has happen: %s\n", BinErrorToStr(err));
-    }
+	if (err != BIN_E_OK)
+	{
+		return fprintf(stderr, "Something wrong has happen: %s\n", BinErrorToStr(err));
+	}
 
-    assert(err == BinGetLastError(&ctx));
+	assert(err == BinGetLastError(&ctx));
 
-    BinParser(&ctx);
+	BinParser(&ctx);
 
-    err = BinGetLastError(&ctx);
-    
-    if (err != BIN_E_OK)
-    {
-        return fprintf(stderr, "Something wrong has happen: %s\n", BinErrorToStr(err));
-    }
+	err = BinGetLastError(&ctx);
+	
+	if (err != BIN_E_OK)
+	{
+		return fprintf(stderr, "Something wrong has happen: %s\n", BinErrorToStr(err));
+	}
 
-    printf("%s was been opened for parser\n", BinGetFilename(&ctx));
+	printf("%s was been opened for parser\n", BinGetFilename(&ctx));
 
-    printf("Binary size: %ld\n", BinGetObjectSize(&ctx));
+	printf("Binary size: %ld\n", BinGetObjectSize(&ctx));
 
-    printf("Binary type is: %s\n", 
-        BinObjectTypeToStr(BinObjectGetType(&ctx))
-    );
+	printf("Binary type is: %s\n", 
+		BinObjectTypeToStr(BinObjectGetType(&ctx))
+	);
 
-    BinFinish(&ctx);
+	BinFinish(&ctx);
 
-    return err;
+	return err;
 }
 
