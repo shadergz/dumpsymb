7d77e43bf661b337a20e46a1bbd91db7249120c3
Gabriel Correia <gabrielcorreia01012020@hotmail.com>
2022-09-04 15:25:51 -0300

    Using spaces instead of tabs

--- dsym.c
+++ dsym.c
@@ -5,43 +5,43 @@
 
 int main(int argc, char *argv[])
 {
-	if (argc < 2) {
-		return argc;
-	}
+    if (argc < 2) {
+        return argc;
+    }
 
-	BinCtx_t ctx;
-	BinError_e err;
+    BinCtx_t ctx;
+    BinError_e err;
 
-	bin_load_file(argv[1], &ctx);
+    bin_load_file(argv[1], &ctx);
 
-	err = bin_error_get_last(&ctx);
+    err = bin_error_get_last(&ctx);
 
-	if (err != BIN_E_OK) {
-		return fprintf(stderr, "Something wrong has happen: %s\n", bin_error_to_str(err));
-	}
+    if (err != BIN_E_OK) {
+        return fprintf(stderr, "Something wrong has happen: %s\n", bin_error_to_str(err));
+    }
 
-	/* Assert test */
-	assert(err == bin_error_get_last(&ctx));
+    /* Assert test */
+    assert(err == bin_error_get_last(&ctx));
 
-	bin_parser(&ctx);
+    bin_parser(&ctx);
 
-	err = bin_error_get_last(&ctx);
-	
-	if (err != BIN_E_OK) {
-		return fprintf(stderr, "Something wrong has happen: %s\n", bin_error_to_str(err));
-	}
-	printf("%s was been opened for parser\n", bin_obj_get_filename(&ctx));
-	printf("Binary size: %ld\n", bin_obj_get_size(&ctx));
+    err = bin_error_get_last(&ctx);
+    
+    if (err != BIN_E_OK) {
+        return fprintf(stderr, "Something wrong has happen: %s\n", bin_error_to_str(err));
+    }
+    printf("%s was been opened for parser\n", bin_obj_get_filename(&ctx));
+    printf("Binary size: %ld\n", bin_obj_get_size(&ctx));
 
-	printf("Binary type is: %s\n", 
-		bin_obj_type_to_str(bin_obj_get_type(&ctx))
-	);
+    printf("Binary type is: %s\n", 
+        bin_obj_type_to_str(bin_obj_get_type(&ctx))
+    );
 
-	printf("Binary endianness: %s\n", bin_cpu_endian_to_str(bin_obj_get_endian(&ctx)));
-	printf("Binary arch: %s\n", bin_class_to_str(bin_obj_get_class(&ctx)));
+    printf("Binary endianness: %s\n", bin_cpu_endian_to_str(bin_obj_get_endian(&ctx)));
+    printf("Binary arch: %s\n", bin_class_to_str(bin_obj_get_class(&ctx)));
 
-	/* bin_unload call isn't required */
-	bin_finish(&ctx);
-	return err;
+    /* bin_unload call isn't required */
+    bin_finish(&ctx);
+    return err;
 }
 
