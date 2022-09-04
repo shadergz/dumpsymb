0576e605a168b22939fde4ffb158e24b5e892296
Gabriel Correia <gabrielcorreia01012020@hotmail.com>
2022-09-04 14:45:27 -0300

    Using new functions

--- dsym.c
+++ dsym.c
@@ -5,18 +5,18 @@
 
 int main(int argc, char *argv[])
 {
-	if (argc < 2)
+	if (argc < 2) {
 		return argc;
+	}
 
 	BinCtx_t ctx;
-	BinError_t err;
+	BinError_e err;
 
 	bin_load_file(argv[1], &ctx);
 
 	err = bin_error_get_last(&ctx);
 
-	if (err != BIN_E_OK)
-	{
+	if (err != BIN_E_OK) {
 		return fprintf(stderr, "Something wrong has happen: %s\n", bin_error_to_str(err));
 	}
 
@@ -27,22 +27,21 @@ int main(int argc, char *argv[])
 
 	err = bin_error_get_last(&ctx);
 	
-	if (err != BIN_E_OK)
-	{
+	if (err != BIN_E_OK) {
 		return fprintf(stderr, "Something wrong has happen: %s\n", bin_error_to_str(err));
 	}
-
 	printf("%s was been opened for parser\n", bin_obj_get_filename(&ctx));
-
 	printf("Binary size: %ld\n", bin_obj_get_size(&ctx));
 
 	printf("Binary type is: %s\n", 
 		bin_obj_type_to_str(bin_obj_get_type(&ctx))
 	);
 
+	printf("Binary endianness: %s\n", bin_cpu_endian_to_str(bin_obj_get_endian(&ctx)));
+	printf("Binary arch: %s\n", bin_class_to_str(bin_obj_get_class(&ctx)));
+
 	/* bin_unload call isn't required */
 	bin_finish(&ctx);
-
 	return err;
 }
 
